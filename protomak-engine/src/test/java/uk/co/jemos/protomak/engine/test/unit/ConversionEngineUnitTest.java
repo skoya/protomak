414641c7b34818197b3131de113b795e572fcfe5
Marco Tedone <marco.tedone@gmail.com>
2011-05-19 22:06:13 +0100

    Added first failing test

---
+++ protomak-engine/src/test/java/uk/co/jemos/protomak/engine/test/unit/ConversionEngineUnitTest.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package uk.co.jemos.protomak.engine.test.unit;
+
+import org.junit.Test;
+
+/**
+ * Unit Tests for the conversion engine.
+ * 
+ * @author mtedone
+ * 
+ */
+public class ConversionEngineUnitTest {
+
+	//------------------->> Constants
+
+	//------------------->> Instance / Static variables
+
+	//------------------->> Constructors
+
+	//------------------->> Public methods
+
+	@Test
+	public void testXsdToProtoConversionEngineForNonExistingFile() {
+
+	}
+
+	// ------------------->> Getters / Setters
+
+	//------------------->> Private methods
+
+	//------------------->> equals() / hashcode() / toString()
+
+	//------------------->> Inner classes
+
+}
