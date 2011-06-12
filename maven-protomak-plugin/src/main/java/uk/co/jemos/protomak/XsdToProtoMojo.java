2e1bc0efad9830b0523334e161d2b298a276bbf4
Marco Tedone <marco.tedone@gmail.com>
2011-06-12 14:55:16 +0100

    PMK-24 Created the Protomak Maven Plugin

---
+++ maven-protomak-plugin/src/main/java/uk/co/jemos/protomak/XsdToProtoMojo.java
@@ -0,0 +1,43 @@
+package uk.co.jemos.protomak;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+
+import uk.co.jemos.protomak.engine.api.ConversionService;
+import uk.co.jemos.protomak.engine.impl.XsomXsdToProtoDomainConversionServiceImpl;
+
+/**
+ * Goal which triggers the generation of proto files from an XSD file
+ * 
+ * @goal xsd-to-proto
+ * 
+ * @phase process-sources
+ */
+public class XsdToProtoMojo extends AbstractMojo {
+
+	/**
+	 * Location of the file.
+	 * 
+	 * @parameter expression="${inputXsdFile}"
+	 * @required
+	 */
+	private String inputXsdFile;
+
+	/**
+	 * The location where the generated files will be placed.
+	 * 
+	 * @parameter expression="${outputFolder}"
+	 * @required
+	 */
+	private String outputFolder;
+
+	public void execute() throws MojoExecutionException {
+
+		//Will use the default serialisation strategy
+		ConversionService conversionService = new XsomXsdToProtoDomainConversionServiceImpl();
+		getLog().info("Invoking the XSOM conversion from XSD to Proto...");
+		conversionService.generateProtoFiles(inputXsdFile, outputFolder);
+		getLog().info("XSOM conversion from XSD to Proto completed successfully");
+
+	}
+}
